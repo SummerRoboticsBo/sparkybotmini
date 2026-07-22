e9e2bf5c6672ff66d912159cfc0277548868f152
SummerRoboticsBo <biglandcap@gmail.com>
2026-07-21 18:47:10 -0700

    Add barebone straight drive example

---
+++ drive_straight.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import time
+from sparkybotmini import SparkyBotMini
+
+robot = SparkyBotMini(port="/dev/ttyUSB0")
+robot.connect()
+robot.set_motor(50, 50, 50, 50)
+time.sleep(5)
+robot.set_motor(0, 0, 0, 0)
+robot.disconnect()
